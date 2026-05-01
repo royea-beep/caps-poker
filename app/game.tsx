9790035276bda30b1db6ef9944090c5f81f35dab
royea-beep <royearguan@gmail.com>
2026-05-01 10:14:52 +0300

    revert: remove WinCelebration mount landscape - bundler issue

--- app/game.tsx
+++ app/game.tsx
@@ -1047,7 +1047,7 @@ function GameScreenInner() {
       {showSafeReveal && (
         <BoardReveal boards={pendingRevealBoards} onDone={onRevealDone} revealSpeed={config.revealSpeed} />
       )}
-      <WinCelebration boardsWon={boardsWonCount} active={celebrateActive} onComplete={() => setCelebrateActive(false)} />
+
       </SafeAreaView>
     );
   }
